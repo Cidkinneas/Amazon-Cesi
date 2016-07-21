763aa45c474b1198f2f5752156a6b56505c12fd9
dsebille <d.sebille83@gmail.com>
2016-07-21 16:27:12 +0200

    travis init

--- src/test/java/amazon/api/BookControllerTest.java
+++ src/test/java/amazon/api/BookControllerTest.java
@@ -15,11 +15,12 @@ public class BookControllerTest {
 	public void setUp() {
 		bc = new BookController();
 	}
+	
 	@Test
 	public void testGetAllBook() {
 		System.out.println("testGetAllBook");
 		
-		String expected = "Book [title=Super livre, author=Marc Dupont, publicationDate=" + new Date(2016,5,10) + ", asin=ISBN1251, publisher=" + publisher + ", pageNumber=220]";
+		String expected = "Book [title=Super livre, author=Marc Dupont, publicationDate=" + new Date(2016,5,10) + ", asin=ISBN1251, publisher=Packt Publication, pageNumber=220]";
 		String result = this.bc.fetchById("ISBN1251");
 		
 		assertEquals(expected, result);
